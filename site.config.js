1c59590d70d61d9b678b82bf03150a3b25d49927
Raccoon <ikhode.station@gmail.com>
2025-03-24 16:33:05 +0700

    update config

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "Ikhode Studio",
+    image: "/avatar_logo.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Game Developer",
+    bio: "Just do what you love, and try your best",
+    email: "ikhode.station@gmail.com",
+    linkedin: "ikhode_studio",
+    github: "MyKhode",
     instagram: "",
   },
   projects: [
@@ -18,19 +18,20 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Ikhode Studio Blog Post Community",
+    description: "Small blog post community platform for software developer who want to share their knowledge and contribute to the open source community.",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://blog.ikhode.site",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
+    // @ts-ignore
     pageId: process.env.NOTION_PAGE_ID,
   },
 
@@ -38,24 +39,28 @@ const CONFIG = {
   googleAnalytics: {
     enable: false,
     config: {
+      // @ts-ignore
       measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
     },
   },
   googleSearchConsole: {
     enable: false,
     config: {
+      // @ts-ignore
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
   },
   naverSearchAdvisor: {
     enable: false,
     config: {
+      // @ts-ignore
       siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
     },
   },
   utterances: {
     enable: true,
     config: {
+      // @ts-ignore
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
       label: "💬 Utterances",
@@ -68,6 +73,7 @@ const CONFIG = {
       appid: "", // Embed Code -> data-app-id value
     },
   },
+  // @ts-ignore
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
